66be1a2b7ce54d027923161b20ef6a7d9c60f602
Eduardo Hollanda Leite <eduardohollanda14@Gmail.com>
2020-03-28 00:25:29 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,4 +8,9 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
